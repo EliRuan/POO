1c5b5b6ba3ba8ccc9df8bb58c9ec12075e51f8e1
Eli Ruan Carvalho <61792997+EliRuan@users.noreply.github.com>
2022-08-12 12:48:51 -0300

    Add files via upload

---
+++ Lista 01 2022/q4.js
@@ -0,0 +1,43 @@
+var mes;
+
+mes = Number(window.prompt('Digite o mês (1 a 12): '));
+
+if (mes == 1) {
+    window.alert('janeiro');
+}
+else if (mes == 2) {
+    window.alert('fevereiro');
+}
+else if (mes == 3) {
+    window.alert('março');
+}
+else if (mes == 4) {
+    window.alert('abril');
+}
+else if (mes == 5) {
+    window.alert('maio');
+}
+else if (mes == 6) {
+    window.alert('junho');
+}
+else if (mes == 7) {
+    window.alert('julho');
+}
+else if (mes == 8) {
+    window.alert('agosto');
+}
+else if (mes == 9) {
+    window.alert('setembro');
+}
+else if (mes == 10) {
+    window.alert('outubro');
+}
+else if (mes == 11) {
+    window.alert('novembro');
+}
+else if (mes == 12) {
+    window.alert('dezembro');
+}
+else{
+    window.alert('Número incorreto. Tente novamente.');
+}
