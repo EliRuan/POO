107ff6202ac079ae372c4b1d9fb588110003dcbf
Eli Ruan Carvalho <61792997+EliRuan@users.noreply.github.com>
2022-09-28 11:59:34 -0300

    Add files via upload

---
+++ Exercicio03/q1.js
@@ -0,0 +1,13 @@
+function ParImp(numero) {
+    return numero;
+}
+var numero = 11;
+if (numero % 2 == 0) {
+    console.log("O número é PAR");
+}
+else if (numero % 2 == 1) {
+    console.log("O número é IMPAR");
+}
+else {
+    console.log("O número é NULO");
+}
---
+++ Exercicio03/q1.ts
@@ -0,0 +1,21 @@
+function ParImp(numero: number)
+{
+    return numero;
+}
+var numero: number = 10;
+
+if (numero % 2 == 0)
+{
+    console.log("O número é PAR"); 
+    
+}
+else if (numero % 2 == 1)
+{
+    console.log("O número é IMPAR"); 
+    
+}
+else
+{
+    console.log("O número é NULO"); 
+    
+}
