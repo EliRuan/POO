58a6c8cca1ab9c718c9be4991f0c50ed840f4e9b
Eli Ruan Carvalho <61792997+EliRuan@users.noreply.github.com>
2022-11-02 20:44:13 -0300

    Add files via upload

---
+++ Exercicio07/q2.ts
@@ -0,0 +1,13 @@
+class Calc {
+    private operando1: number
+    private operando2: number
+
+    somar(operando1, operando2): number {
+        return this.operando1 + this.operando2
+    }
+
+    constructor(operando1, operando2) {
+        this.operando1 = 0
+        this.operando2 = 0
+    }
+}
